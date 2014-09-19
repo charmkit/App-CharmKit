ec3de098528cfc7c0d1f826e3414f31a01447d3d
Adam Stokes <adam.stokes@ubuntu.com>
2014-09-19 14:47:54 -0400

    hook generate task
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- lib/App/CharmKit/Command/generate.pm
+++ lib/App/CharmKit/Command/generate.pm
@@ -4,29 +4,33 @@ package App::CharmKit::Command::generate;
 
 use App::CharmKit -command;
 use Moo;
+with('App::CharmKit::Role::Generate');
 use namespace::clean;
 
 sub opt_spec {
     return (
-        [   "t|type",
-            "generate skelton of type (application, library, or none(default))"
+        [   "relation|r",
+            "generate a relation hook"
         ]
     );
 }
 
 sub abstract { 'Generate a charm skeleton.'}
-sub usage_desc {'%c generate -t|--type=library <charm-name>'}
+sub usage_desc {'%c generate [-r] <hook-name>'}
 
 sub validate_args {
     my ($self, $opt, $args) = @_;
-    $self->usage_error(
-        "Incorrect type specified, must be application, library, or none.")
-      unless $opt->{type} =~ /^application|library|none/;
+    $self->usage_error("Must be a hook of name install, "
+          . "config-changed, start, start, upgrade-charm")
+      unless $opt->{relation}
+      || $args->[0] =~ /^install|config-changed|start|stop|upgrade-charm/;
 }
 
 sub execute {
     my ($self, $opt, $args) = @_;
-    printf("Running generate.");
+    printf("Generating %s hook ..\n", $args->[0]);
+    $self->create_hook($args->[0]);
+    printf("Done.\n");
 }
 
 1;
--- lib/App/CharmKit/Role/Generate.pm
+++ lib/App/CharmKit/Role/Generate.pm
@@ -2,7 +2,22 @@ package App::CharmKit::Role::Generate;
 
 # ABSTRACT: Generators for common tasks
 
+use Path::Tiny;
 use Moo::Role;
+
+=attr src
+
+Source directory for hooks
+
+=cut
+has src => (
+    is      => 'ro',
+    lazy    => 1,
+    default => sub {
+        path('.')->child('src/hooks');
+    }
+);
+
 
 =method create_hook(STR hook)
 
@@ -11,6 +26,34 @@ Creates a charm hook based on `hook` name
 =cut
 sub create_hook {
     my ($self, $hook) = @_;
+
+    (my $hook_heading = qq{#!/usr/bin/env perl
+
+# PODNAME: $hook
+
+use App::CharmKit::Helpers;
+
+# begin your $hook below
+
+
+
+# end hook code
+
+__END__
+
+=begin
+
+=head1 $hook
+
+Description of hook
+
+=head1 SYNOPSIS
+
+Any usage here
+
+=cut
+});
+    $self->src->child($hook)->spew_utf8($hook_heading);
 }
 
 1;
