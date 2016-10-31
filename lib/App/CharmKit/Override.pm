c820eef3340dae44c99588e70d35e04d589e9151
Adam Stokes <battlemidget@users.noreply.github.com>
2016-10-31 16:05:03 -0400

    update changes
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- lib/App/CharmKit/Override.pm
+++ lib/App/CharmKit/Override.pm
@@ -2,7 +2,7 @@ package App::CharmKit::Override;
 
 =head1 NAME
 
-App::CharmKit:: - additional sugar
+App::CharmKit::Override
 
 =cut
 
@@ -16,17 +16,50 @@ use base "Exporter::Tiny";
 
 our @EXPORT = qw(config resource unit status);
 
+=over
+
+=item config($key)
+
+This queries the charms config
+
+=cut
+
 sub config($key) {
     return run "config-get $key";
 }
+
+=item resource($key)
+
+Pulls the resource bound to $key
+
+=cut
 
 sub resource($key) {
     return run "resource-get $key";
 }
 
+=item unit($key)
+
+Queries the Juju unit for a specific value
+
+C<unit 'public-address';>
+
+This above code would pull the public-address of the unit in the context of the
+running charm
+
+=cut
+
 sub unit($key){
     return run "unit-get $key";
 }
+
+=item status($level, $msg)
+
+Sets the charm's current status of execution
+
+=cut
+
+=back
 
 sub status($level="active", $msg="Ready") {
     return run "status-set $level $msg";
