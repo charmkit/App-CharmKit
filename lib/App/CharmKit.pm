72fd87fffcea330c911bf7aa4651dabe4edd3705
Adam Stokes <battlemidget@users.noreply.github.com>
2018-11-24 17:34:31 -0500

    more cleanups
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- lib/App/CharmKit.pm
+++ lib/App/CharmKit.pm
@@ -1,70 +1,14 @@
 # ABSTRACT: Additional helper hook routines
 package App::CharmKit;
 
-our $VERSION = '2.09';
+our $VERSION = '2.10';
 
-use strict;
-use warnings;
-no warnings 'experimental::signatures';
-use feature 'signatures';
-use Rex::Commands::Run;
+use charm;
+
 use FindBin;
 use base "Exporter::Tiny";
 
-our @EXPORT = qw(config resource unit status plugin);
-
-=over 4
-
-=item config($key)
-This queries the charms config
-
-=cut
-
-sub config ($key) {
-    return run "config-get $key";
-}
-
-=item resource($key)
-
-Pulls the resource bound to $key
-
-=cut
-
-sub resource ($key) {
-    return run "resource-get $key";
-}
-
-=item unit($key)
-
-Queries the Juju unit for a specific value
-C<unit 'public-address';>
-This above code would pull the public-address of the unit in the context of the
-running charm
-
-=cut
-
-sub unit ($key) {
-    return run "unit-get $key";
-}
-
-=item status($level, $msg)
-
-Sets the charm's current status of execution
-
-=cut
-
-
-sub status ($level = "active", $msg = "Ready") {
-    return run "status-set $level $msg";
-}
-
-=item plugin($name, %opts)
-
-Load a plugin, optionally passing options
-
-=back
-
-=cut
+our @EXPORT = qw(plugin);
 
 sub plugin ($name, $opts = {}) {
     my $name_path = "$FindBin::Bin/../lib/$name.pm";
